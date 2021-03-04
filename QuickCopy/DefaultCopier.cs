e72f6dc7fe06503422d891c0aef2167deb1ac94e
Jon Fast <jnfstdj656@gmail.com>
2021-03-04 18:51:14 -0500

    default copier.

--- QuickCopy/DefaultCopier.cs
+++ QuickCopy/DefaultCopier.cs
@@ -19,11 +19,6 @@ namespace QuickCopy
 
         public ProgramOptions Options { get; set; }
 
-        public void Copy()
-        {
-            DirectoryCopy(Options.SourceDirectory, Options.TargetDirectory, true, true);
-        }
-
         public void IncrementalCopy(List<FileInfoParserAction> actions)
         {
             var skipFolders = Options.SkipFolders
@@ -62,8 +57,10 @@ namespace QuickCopy
                             Log.Info($"Copied {action.Source.GetPath()}");
                         }
                         else
-                            DirectoryCopy(action.Source.GetPath(), 
-                                destinationSegment, true, false);
+                        {
+                            Directory.CreateDirectory(action.Destination.GetPath());
+                        }
+
                         break;
                     case ActionType.Update:
                         foreach (var skipFolder in skipFolders
@@ -81,8 +78,10 @@ namespace QuickCopy
                             Log.Info($"Copied {action.Source.GetPath()} (changed)");
                         }
                         else
-                            DirectoryCopy(action.Source.GetPath(),
-                                action.Destination.GetPath(), true, false);
+                        {
+                            Directory.CreateDirectory(action.Destination.GetPath());
+                        }
+
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
@@ -105,43 +104,6 @@ namespace QuickCopy
                             Directory.Delete(action.Destination.GetPath(), true);
                         break;
                 }
-        }
-
-        private static void DirectoryCopy(string sourceDirName, string destDirName, bool recursive, bool cleanFolder)
-        {
-            var dir = new DirectoryInfo(sourceDirName);
-            var destDir = new DirectoryInfo(destDirName);
-
-            if (!dir.Exists)
-                throw new DirectoryNotFoundException(
-                    "Source directory does not exist or could not be found: "
-                    + sourceDirName);
-
-            if (cleanFolder)
-            {
-                Directory.Delete(destDir.FullName, true);
-                Directory.CreateDirectory(destDir.FullName);
-            }
-
-            var dirs = dir.GetDirectories();
-
-            Directory.CreateDirectory(destDirName);
-
-            var files = dir.GetFiles();
-            foreach (var file in files)
-            {
-                var tempPath = Path.Combine(destDirName, file.Name);
-                file.CopyTo(tempPath, true);
-                Log.Info($"Copied {tempPath}");
-            }
-
-            if (!recursive) return;
-
-            foreach (var subDirectory in dirs)
-            {
-                var tempPath = Path.Combine(destDirName, subDirectory.Name);
-                DirectoryCopy(subDirectory.FullName, tempPath, true, false);
-            }
         }
     }
 }
