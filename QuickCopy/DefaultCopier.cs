cfd31d72bafce7d68da8aa05162d39426dc28fb6
Jon Fast <jnfstdj656@gmail.com>
2021-03-04 18:52:15 -0500

    default copier updated.

--- QuickCopy/DefaultCopier.cs
+++ QuickCopy/DefaultCopier.cs
@@ -19,6 +19,11 @@ namespace QuickCopy
 
         public ProgramOptions Options { get; set; }
 
+        public void Copy()
+        {
+            DirectoryCopy(Options.SourceDirectory, Options.TargetDirectory, true, true);
+        }
+
         public void IncrementalCopy(List<FileInfoParserAction> actions)
         {
             var skipFolders = Options.SkipFolders
@@ -104,6 +109,43 @@ namespace QuickCopy
                             Directory.Delete(action.Destination.GetPath(), true);
                         break;
                 }
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool recursive, bool cleanFolder)
+        {
+            var dir = new DirectoryInfo(sourceDirName);
+            var destDir = new DirectoryInfo(destDirName);
+
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+
+            if (cleanFolder)
+            {
+                Directory.Delete(destDir.FullName, true);
+                Directory.CreateDirectory(destDir.FullName);
+            }
+
+            var dirs = dir.GetDirectories();
+
+            Directory.CreateDirectory(destDirName);
+
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                var tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, true);
+                Log.Info($"Copied {tempPath}");
+            }
+
+            if (!recursive) return;
+
+            foreach (var subDirectory in dirs)
+            {
+                var tempPath = Path.Combine(destDirName, subDirectory.Name);
+                DirectoryCopy(subDirectory.FullName, tempPath, true, false);
+            }
         }
     }
 }
